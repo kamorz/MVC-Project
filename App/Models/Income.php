ce7e71f307e611df91a13835707d13a462b1bdf1
Kamil Orzechowski <kamil.orzechowski.programista@gmail.com>
2020-05-29 11:42:46 +0200

    Ascribing corrected income id in database

--- App/Models/Income.php
+++ App/Models/Income.php
@@ -18,10 +18,14 @@ class Income extends \Core\Model
 	public function add()
     {
 			$user_id = $_SESSION['user_id'];
+			$category_id = static::findCategoryID($user_id, $this->category);
 			
-            $sql = 'INSERT INTO incomes (user_id, amount, date_of_income, income_comment)
-            VALUES (:user_id, :cash, :date, :addDesc)';
-
+			//$message = $category_id;
+			//echo "<script type='text/javascript'>alert('$message');</script>";
+			
+            $sql = 'INSERT INTO incomes (user_id, amount, date_of_income, income_comment, income_category_assigned_to_user_id)
+            VALUES (:user_id, :cash, :date, :addDesc, :category_id)';  
+			
             $db = static::getDB();
             $stmt = $db->prepare($sql);
 
@@ -29,24 +33,31 @@ class Income extends \Core\Model
             $stmt->bindValue(':cash', $this->cash, PDO::PARAM_STR);
             $stmt->bindValue(':date', $this->date, PDO::PARAM_STR);
             $stmt->bindValue(':addDesc', $this->addDesc, PDO::PARAM_STR);
+            $stmt->bindValue(':category_id', $category_id, PDO::PARAM_INT); 
 
             return $stmt->execute();
+    }
+	
+	
+	 public static function findCategoryID($user_id, $category)
+    {
+        $sql = 'SELECT id FROM incomes_category_assigned_to_users WHERE user_id = :user_id AND name = :category ';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+        $stmt->bindValue(':category', $category, PDO::PARAM_STR);
+
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+
+        $stmt->execute();
+		
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		
+		$message = $row['id'];
+		echo "<script type='text/javascript'>alert('$message');</script>";
+		
+		return $row['id'];
+
     }
 }
-
-/*	public function add()
-    {
-            $sql = 'INSERT INTO incomes (id, user_id, income_category_assigned_to_user_id, amount, date_of_income, income_comment)
-            VALUES (NULL, "1", "1", :cash, :date, :addDesc)';
-
-            $db = static::getDB();
-            $stmt = $db->prepare($sql);
-
-            $stmt->bindValue(':category', $this->category, PDO::PARAM_STR);
-            $stmt->bindValue(':cash', $this->cash, PDO::PARAM_STR);
-            $stmt->bindValue(':date', $this->date, PDO::PARAM_STR);
-            $stmt->bindValue(':addDesc', $this->addDesc, PDO::PARAM_STR);
-
-            return $stmt->execute();
-    }
-	*/
