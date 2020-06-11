3855e780d0c62e1178352d70efe3949cd025c247
Kamil Orzechowski <kamil.orzechowski.programista@gmail.com>
2020-06-11 18:59:11 +0200

    Correct displaying income datas on table

--- App/Models/Overview.php
+++ App/Models/Overview.php
@@ -12,7 +12,9 @@ class Overview extends \Core\Model
     {
 		$id = ($_SESSION['user_id']);
 		$currentMonth = date("m");
-		$arrayA = [];
+		$result = [];
+		$result_full = [];
+		$i=0;
 		
         $sql = "SELECT * FROM incomes WHERE user_id = :id AND EXTRACT(month FROM date_of_income) = '$currentMonth'";
 		
@@ -25,13 +27,31 @@ class Overview extends \Core\Model
 
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
 		{
+			//$new = array_push($result, $row['date_of_income'], $row['income_category_assigned_to_user_id'], $row['amount'], $row['income_comment']);
 			$result['date'] = $row['date_of_income'];
-			$result['category'] = $row['income_category_assigned_to_user_id'];
+			$result['category'] = static::findIncomeCategoryName($row['income_category_assigned_to_user_id']);
 			$result['amount'] = $row['amount'];
-			$result['comment'] = $row['income_comment'];		
+			$result['comment'] = $row['income_comment'];	
+			$result_full[$i] = $result;
+			$i++;
 		}	 
-		return $result;
+		return $result_full;
 		
-		//return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+	
+		public static function findIncomeCategoryName($id)
+    {
+        $sql = 'SELECT name FROM incomes_category_assigned_to_users WHERE id = :id';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+        $stmt->execute();	
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		
+		return $row['name'];
+
     }
 }
