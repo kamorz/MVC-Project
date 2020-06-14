8688c9a9f2768397ff7af2ef3c9be021e962176c
Kamil Orzechowski <kamil.orzechowski.programista@gmail.com>
2020-06-14 10:41:15 +0200

    Visual changes

--- App/Controllers/Balance.php
+++ App/Controllers/Balance.php
@@ -29,6 +29,8 @@ class Balance extends Authenticated
 		$incomeCategoriesAssignedToUser= Overview:: findAllIncomeCategoriesAssignedToUser('1');
 		$arg['incomes']= Overview::findIncomesFromCurrentMonthInDatabase('1');
 		$arg['incomeTypes']= $incomeCategoriesAssignedToUser;
+		$dt = "2008-02-23";
+		$arg['proba']='First day : '. date("Y-m-01", strtotime($dt)).' - Last day : '. date("Y-m-t", strtotime($dt));
 		
 		$expenseCategoriesAssignedToUser= Overview:: findAllExpenseCategoriesAssignedToUser('1');
 		$arg['expenses']= Overview::findExpensesFromCurrentMonthInDatabase('1');
